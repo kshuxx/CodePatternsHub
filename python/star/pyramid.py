a7e92fb5da16a4a190f7799b2591c655a4357fde
Shubhanshu <kshuxx@gmail.com>
2024-09-22 07:04:06 +0530

    UPDATE comment

--- python/star/pyramid.py
+++ python/star/pyramid.py
@@ -1,4 +1,4 @@
-# Define a function to print a pyramid
+# Define a function to print a pyramid using stars
 def pyramid(n):
     # Determine the number of columns for the pyramid pattern
     col = 2 * n - 1  # This ensures the pattern is symmetrical for both even and odd numbers of rows
